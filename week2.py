d12c0733719a192d6c278ac64fff74f81b8924b4
jarjapar <you@example.com>
2025-03-12 21:04:10 +0800

    2

--- week2.py
+++ week2.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QHBoxLayout, QGroupBox
 
 class UserRegistrationForm(QWidget):
     def __init__(self):
@@ -8,6 +8,24 @@ class UserRegistrationForm(QWidget):
 
     def initUI(self):
         main_layout = QVBoxLayout()
+
+        identitas_group = QGroupBox("Identitas")
+        identitas_layout = QVBoxLayout()
+        identitas_layout.addWidget(QLabel("Nama : Muhammad Fajar Maulana"))
+        identitas_layout.addWidget(QLabel("NIM : F1D022072"))
+        identitas_layout.addWidget(QLabel("Kelas : D"))
+        identitas_group.setLayout(identitas_layout)
+
+        nav_group = QGroupBox("Navigation")
+        nav_layout = QHBoxLayout()
+        nav_layout.addWidget(QPushButton("Home"))
+        nav_layout.addWidget(QPushButton("About"))
+        nav_layout.addWidget(QPushButton("Contact"))
+        nav_group.setLayout(nav_layout)
+
+        main_layout.addWidget(identitas_group)
+        main_layout.addWidget(nav_group)
+
         self.setLayout(main_layout)
         self.setWindowTitle("Week 2 : Layout - User Registration Form")
         self.setGeometry(100, 100, 500, 400)
